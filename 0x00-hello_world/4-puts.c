012a510d6c5114eb58b6fee7ae8e18dede85c03c
Anthony Oloyede <oloyede.tony@gmail.com>
2022-09-08 20:06:57 -0700

    size

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-
 /**
 *print string in the put function
 *using the main function
-*program ends with 0
+*program ends 
+*return : 0
 */
 
-int main(void){
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return 0;
 }
